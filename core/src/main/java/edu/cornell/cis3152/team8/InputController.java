5371eae6dbab5be08464cd5a015aa76c37b65a5c
Luke Tao <lyt5@cornell.edu>
2025-03-09 16:52:29 -0400

    Add control codes to InputController

--- core/src/main/java/edu/cornell/cis3152/team8/InputController.java
+++ core/src/main/java/edu/cornell/cis3152/team8/InputController.java
@@ -1,5 +1,19 @@
 package edu.cornell.cis3152.team8;
 
 public interface InputController {
-    public int getAction();
+    // Constants for the control codes
+    /** Do not do anything */
+    int CONTROL_NO_ACTION  = 0x00;
+    /** Move to the left */
+    int CONTROL_MOVE_LEFT  = 0x01;
+    /** Move to the right */
+    int CONTROL_MOVE_RIGHT = 0x02;
+    /** Move up */
+    int CONTROL_MOVE_UP    = 0x04;
+    /** Move down */
+    int CONTROL_MOVE_DOWN  = 0x08;
+    /** Use ability */
+    int CONTROL_USE_ABILITY = 0x10;
+
+    int getAction();
 }
