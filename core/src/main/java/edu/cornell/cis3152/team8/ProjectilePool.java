4d7c4df8ba2d864137080ddb017e97f2d471956d
Vinny <vh222@cornell.edu>
2025-03-13 16:53:20 -0400

    Rethinking ProjectilePool
    + with the Projectile rewrite/refactor, need to rethink how the ProjectilePool will work

--- core/src/main/java/edu/cornell/cis3152/team8/ProjectilePool.java
+++ core/src/main/java/edu/cornell/cis3152/team8/ProjectilePool.java
@@ -1,11 +1,3 @@
 package edu.cornell.cis3152.team8;
 
-import java.util.Iterator;
 
-public class ProjectilePool implements Iterable<Projectile> {
-    protected int size;
-
-    public Iterator<Projectile> iterator() {
-        // Awaiting projectile iterator, satisfying the Iterable interface
-    }
-}
