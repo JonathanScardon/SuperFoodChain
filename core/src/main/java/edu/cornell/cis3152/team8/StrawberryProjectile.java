64164eaad92c4169cd4ecb29624591f666e067fe
Vinny <vh222@cornell.edu>
2025-03-16 14:25:59 -0400

    Sample implementations for companion specific projectiles
    + added simple companion specific projectile subclasses
    + the subclasses just call the Projectile constructor with the position

---
+++ core/src/main/java/edu/cornell/cis3152/team8/StrawberryProjectile.java
@@ -0,0 +1,13 @@
+package edu.cornell.cis3152.team8;
+
+/**
+ * Projectile specific to the Strawberry companion
+ *
+ * The projectile should be shooting a set amount of projectiles in random directions
+ */
+public class StrawberryProjectile extends Projectile {
+    public StrawberryProjectile(float x, float y) {
+        // Call the parent constructor (in Projectile)
+        super(x,y);
+    }
+}
