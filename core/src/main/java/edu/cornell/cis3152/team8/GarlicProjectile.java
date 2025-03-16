863625a9d00f1ffa34538876ab9c1749a3617592
Vinny <vh222@cornell.edu>
2025-03-16 14:25:59 -0400

    Sample implementations for companion specific projectiles
    + added simple companion specific projectile subclasses
    + the subclasses just call the Projectile constructor with the position

---
+++ core/src/main/java/edu/cornell/cis3152/team8/GarlicProjectile.java
@@ -0,0 +1,13 @@
+package edu.cornell.cis3152.team8;
+
+/**
+ * Projectile specific to the Garlic companion
+ *
+ * The projectile should be shooting a projectile that then has a lingering poison cloud effect
+ */
+public class GarlicProjectile extends Projectile{
+    public GarlicProjectile(float x, float y) {
+        // Call parent constructor for now
+        super(x,y);
+    }
+}
