8825d186bddc313665eba46be4b25863da69bb82
Luke Tao <lyt5@cornell.edu>
2025-03-17 12:56:06 -0400

    Basic boss movement and idle

--- core/src/main/java/edu/cornell/cis3152/team8/Boss.java
+++ core/src/main/java/edu/cornell/cis3152/team8/Boss.java
@@ -1,6 +1,33 @@
 package edu.cornell.cis3152.team8;
 
+import com.badlogic.gdx.graphics.Color;
+import edu.cornell.gdiac.graphics.SpriteBatch;
+
 public abstract class Boss extends GameObject {
+    /**
+     * How far forward this boss can move in a single turn
+     */
+    private static float MOVE_SPEED = 3f;
+    /**
+     * The damping factor for deceleration
+     */
+    private static float SPEED_DAMP = 0.75f;
+    /**
+     * How long the boss must wait until it can attack again
+     */
+    private static float COOLDOWN = 60f;
+    /**
+     * An epsilon for float comparison
+     */
+    private static float EPSILON = 0.01f;
+
+    /**
+     * Current amount of time until next attack
+     */
+    private float attackCool;
+
+    protected float angle; // angle of the sprite TEMPORARY
+
     public enum BossType {
         MOUSE,
         CHEF,
@@ -19,6 +46,45 @@ public abstract class Boss extends GameObject {
         return ObjectType.BOSS;
     }
 
+    public void update(int controlCode) {
+        // Determine how we are moving.
+        boolean movingLeft = (controlCode & InputController.CONTROL_MOVE_LEFT) != 0;
+        boolean movingRight = (controlCode & InputController.CONTROL_MOVE_RIGHT) != 0;
+        boolean movingUp = (controlCode & InputController.CONTROL_MOVE_UP) != 0;
+        boolean movingDown = (controlCode & InputController.CONTROL_MOVE_DOWN) != 0;
+
+        // Process movement command.
+        if (movingLeft) {
+            velocity.x = -MOVE_SPEED;
+            velocity.y = 0;
+            angle = 180f;
+        } else if (movingRight) {
+            velocity.x = MOVE_SPEED;
+            velocity.y = 0;
+            angle = 0f;
+        } else if (movingUp) {
+            velocity.y = MOVE_SPEED;
+            velocity.x = 0;
+            angle = 90f;
+        } else if (movingDown) {
+            velocity.y = -MOVE_SPEED;
+            velocity.x = 0;
+            angle = 270f;
+        } else {
+            // NOT MOVING, SO SLOW DOWN
+            velocity.x *= SPEED_DAMP;
+            velocity.y *= SPEED_DAMP;
+            if (Math.abs(velocity.x) < EPSILON) {
+                velocity.x = 0.0f;
+            }
+            if (Math.abs(velocity.y) < EPSILON) {
+                velocity.y = 0.0f;
+            }
+        }
+
+        position.add(velocity);
+    }
+
     public float getHealth() {
         return health;
     }
@@ -28,4 +94,24 @@ public abstract class Boss extends GameObject {
     }
 
     public abstract BossType getBossType();
+
+    /**
+     * Resets the cooldown of the boss attack
+     *
+     * If flag is true, the weapon will cool down by one animation frame.
+     * Otherwise, it will reset to its maximum cooldown.
+     *
+     * @param flag whether to cooldown or reset
+     */
+    public void coolDown(boolean flag) {
+        if (flag && attackCool > 0) {
+            attackCool--;
+        } else if (!flag) {
+            attackCool = COOLDOWN;
+        }
+    }
+
+    public boolean canAttack() {
+        return attackCool <= 0;
+    }
 }
--- core/src/main/java/edu/cornell/cis3152/team8/MouseController.java
+++ core/src/main/java/edu/cornell/cis3152/team8/MouseController.java
@@ -1,5 +1,54 @@
 package edu.cornell.cis3152.team8;
 
-public class MouseController {
+public class MouseController extends BossController {
+    final int RADIUS = 40 * 3 / 2; // TEMPORARY
 
+    public MouseController(Boss boss, GameState gameState) {
+        super(boss, gameState);
+        attack();
+    }
+
+    @Override
+    public int getAction() {
+        ticks++;
+
+        if (ticks % 10 == 0) {
+            // only update state every 10 frames
+            if (state == FSMState.ATTACK) {
+                if (boss.getY() + RADIUS < 0) {
+                    boss.setX(boss.getX() + RADIUS * 2);
+                    action = CONTROL_MOVE_UP;
+                } else if (boss.getY() - RADIUS > 720) {
+                    boss.setX(boss.getX() + RADIUS * 2);
+                    action = CONTROL_MOVE_DOWN;
+                }
+
+                if (boss.getX() - RADIUS > 1280) {
+                    // off the screen on the right, we're done attacking
+                    state = FSMState.IDLE;
+                    boss.setX((float) 1280 / 2);
+                    boss.setY((float) 720 / 2);
+                    action = CONTROL_NO_ACTION;
+                }
+            } else if (state == FSMState.IDLE) {
+                boss.coolDown(true); // decrease attack cooldown
+
+                // try to attack all the time
+                if (boss.canAttack()) {
+                    attack();
+                }
+            }
+        }
+
+        return action;
+    }
+
+    @Override
+    public void attack() {
+        boss.setX(RADIUS);
+        boss.setY(-RADIUS);
+        boss.coolDown(false);
+        state = FSMState.ATTACK;
+        action = CONTROL_MOVE_UP;
+    }
 }
