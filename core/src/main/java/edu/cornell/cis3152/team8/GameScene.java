a6ec6589824bee57abc392f703b9883dfa7c658c
kjp76 <kjp76@cornell.edu>
2025-03-20 16:22:22 -0400

    temp Shield UI

--- core/src/main/java/edu/cornell/cis3152/team8/GameScene.java
+++ core/src/main/java/edu/cornell/cis3152/team8/GameScene.java
@@ -252,12 +252,24 @@ public class GameScene implements Screen {
 
         String coins = "X" + player.getCoins();
         String HP = "Boss HP: " + bosses[0].getHealth();
+        TextLayout shield;
         TextLayout coinCount = new TextLayout(coins, font, 128);
         TextLayout bossHP = new TextLayout(HP, font, 128);
         //Temp UI
         game.batch.draw(coinTexture, 1150, 65, 50, 50);
         game.batch.drawText(bossHP,600,700);
         game.batch.drawText(coinCount, 1200f, 80f);
+
+        if (player.hasShield()){
+            font.setColor(Color.GREEN);
+            shield = new TextLayout("Shield: On", font);
+
+        } else {
+            font.setColor(Color.RED);
+            shield = new TextLayout("Shield: Off", font);
+        }
+        game.batch.drawText(shield,600,20);
+        font.setColor(Color.WHITE);
 
         if (!player.isAlive()) {
             drawLose();
