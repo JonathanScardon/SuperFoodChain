05357c67527bf91643980838cadd86ae36e9a448
Luke Tao <lyt5@cornell.edu>
2025-04-21 12:05:26 -0400

    Move minion and companion spawn to level editor

--- assets/tiled/cutting_board.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="cutting_board" tilewidth="2048" tileheight="2048" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image source="../images/Tile.png" width="1280" height="720"/>
- </tile>
-</tileset>
