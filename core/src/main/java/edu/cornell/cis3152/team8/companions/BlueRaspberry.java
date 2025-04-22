3621c41a808b1c1a0f23db7375c2ad5a74631f10
Luke Tao <lyt5@cornell.edu>
2025-04-21 22:53:49 -0400

    Fix following delay and blue raspberry speed boost

--- core/src/main/java/edu/cornell/cis3152/team8/companions/BlueRaspberry.java
+++ core/src/main/java/edu/cornell/cis3152/team8/companions/BlueRaspberry.java
@@ -56,7 +56,7 @@ public class BlueRaspberry extends Companion {
     public void useAbility(GameState state) {
         usedBoost = true;
         //increase the player speed
-        Companion.setSpeed(Companion.getSpeed() + .5f);
+        Companion.setSpeed(Companion.getSpeed() + 25f);
         Player.calculateDelay();
     }
 }
