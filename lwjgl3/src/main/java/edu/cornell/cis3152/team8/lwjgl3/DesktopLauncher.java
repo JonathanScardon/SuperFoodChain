c5c9b7c6b50747128d295669c867bd7a0d406c95
Luke Tao <lyt5@cornell.edu>
2025-04-27 14:15:22 -0400

    Change name of window

--- lwjgl3/src/main/java/edu/cornell/cis3152/team8/lwjgl3/DesktopLauncher.java
+++ lwjgl3/src/main/java/edu/cornell/cis3152/team8/lwjgl3/DesktopLauncher.java
@@ -34,7 +34,7 @@ public class DesktopLauncher {
      * This method simply exists to start a new GDXApp. For desktop games,
      * LibGDX is built on top of LWJGL (this is not the case for Android).
      *
-     * @param arg Command line arguments
+     * @param args Command line arguments
      */
     public static void main(String[] args) {
         // This handles macOS support and helps on Windows.
@@ -53,7 +53,7 @@ public class DesktopLauncher {
      */
     private static GDXAppSettings configureApplication() {
         GDXAppSettings config = new GDXAppSettings();
-        config.title = "Optimization";
+        config.title = "Super Food Chain";
 
         // Use windowed mode
         config.fullscreen = false;
