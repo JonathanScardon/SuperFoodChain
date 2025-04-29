bc37edb3564d43cd43c1c45eaec53d4a1640e572
kjp76 <kjp76@cornell.edu>
2025-04-29 02:15:49 -0400

    Added multiple level pages

--- core/src/main/java/edu/cornell/cis3152/team8/LevelButton.java
+++ core/src/main/java/edu/cornell/cis3152/team8/LevelButton.java
@@ -1,23 +1,29 @@
 package edu.cornell.cis3152.team8;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Affine2;
+import edu.cornell.gdiac.assets.AssetDirectory;
 import edu.cornell.gdiac.graphics.SpriteBatch;
 import edu.cornell.gdiac.graphics.SpriteBatch.BlendMode;
+import edu.cornell.gdiac.graphics.TextLayout;
 
 public class LevelButton extends Button {
 
-    private Texture number;
+    private TextLayout number;
     private Texture lock;
     private boolean locked;
+    private BitmapFont font;
 
-    public LevelButton(float x, float y, Texture number, int exitCode) {
+    public LevelButton(float x, float y, int exitCode, AssetDirectory assets) {
         super(x, y, new Texture("images/LevelSelectPlate.png"),
             new Texture("images/LevelSelectPlate.png"), exitCode);
         lock = new Texture("images/Lock.png");
-        this.number = number;
-
+        font = assets.getEntry("lpc", BitmapFont.class);
+        font.setColor(Color.BROWN);
+        this.number = new TextLayout(exitCode + "", font);
         locked = true;
-
     }
 
     @Override
@@ -32,7 +38,12 @@ public class LevelButton extends Button {
             batch.draw(lock, posX + texture.getWidth() / 2f - lock.getWidth() / 2f,
                 posY + texture.getHeight() / 2f - lock.getHeight() / 2f);
         } else {
-            batch.draw(number, posX, posY);
+
+            Affine2 trans = new Affine2();
+            trans.setToTrnScl(posX + (texture.getWidth() / 2f - number.getWidth() / 2f),
+                posY + (texture.getHeight() / 2f) - number.getWidth(), 2f, 2f);
+            batch.drawText(number, trans);
+
         }
     }
 
--- core/src/main/java/edu/cornell/cis3152/team8/Settings.java
+++ core/src/main/java/edu/cornell/cis3152/team8/Settings.java
@@ -4,18 +4,25 @@ import com.badlogic.gdx.graphics.Texture;
 import edu.cornell.gdiac.graphics.SpriteBatch;
 
 public class Settings {
-        private Texture background;
-    public Settings(){
+
+    private Texture background;
+    private Texture dim;
+
+    public Settings() {
         background = new Texture("images/SettingsPage.png");
+        dim = new Texture("images/dim.png");
     }
 
-    public void draw(SpriteBatch batch){
-        batch.draw(background,0,0);
+    public void draw(SpriteBatch batch, float page) {
+        float move = 1280 * (page - 1);
+        batch.draw(dim, move, 0);
+        batch.draw(background, 1280 * (page - 1), 0);
         Texture bar = new Texture("images/VolumeBar.png");
-        batch.draw(bar,430,405);
-        batch.draw(bar,430,203);
+        batch.draw(bar, move + 430, 405);
+        batch.draw(bar, move + 430, 203);
     }
-    public void update(){
+
+    public void update() {
 
     }
 }
