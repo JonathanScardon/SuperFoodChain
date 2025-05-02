172d419744930e12bcb8953bbcac61b1a518d300
kjp76 <kjp76@cornell.edu>
2025-05-02 15:39:15 -0400

    Button Texture Updates

--- core/src/main/java/edu/cornell/cis3152/team8/LevelButton.java
+++ core/src/main/java/edu/cornell/cis3152/team8/LevelButton.java
@@ -1,5 +1,6 @@
 package edu.cornell.cis3152.team8;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import edu.cornell.gdiac.assets.AssetDirectory;
 import edu.cornell.gdiac.graphics.SpriteBatch;
@@ -37,6 +38,7 @@ public class LevelButton extends Button {
 
         if (isHovering() && !locked) {
             batch.draw(hover, posX, posY, width, height); //draw hover state
+            number.setColor(new Color(175f / 255, 73f / 255, 0, 1));
         } else {
             batch.draw(texture, posX, posY, width, height); //draw normally
         }
@@ -50,6 +52,7 @@ public class LevelButton extends Button {
                 posY + (height / 2f), 0.0f, 1f, 1f);
             batch.drawText(number, transform);
         }
+        number.setColor(fontColor);
     }
 
     /**
